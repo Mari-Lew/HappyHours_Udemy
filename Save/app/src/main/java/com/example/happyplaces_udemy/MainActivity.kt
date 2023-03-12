de23de2610017c5d7f418962048c543e9652fb18
Marissa Lewinski <101777634+Mari-Lew@users.noreply.github.com>
2023-03-12 11:10:01 -0500

    save work

--- Save/app/src/main/java/com/example/happyplaces_udemy/MainActivity.kt
+++ Save/app/src/main/java/com/example/happyplaces_udemy/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.happyplaces_udemy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.happyplaces_udemy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var binding : ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.addHappyPlaceBtn?.setOnClickListener {
+            Toast.makeText(this@MainActivity,"Button Pressed", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
